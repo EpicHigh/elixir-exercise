09a57a63dab8cf94631fea14e39c10ad6fdeedb7
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2022-01-04 15:01:58 +0700

    📝 Update documentation.

--- lib/bird_count.ex
+++ lib/bird_count.ex
@@ -12,37 +12,36 @@ defmodule BirdCount do
   """
   @spec today(list(number)) :: number
   @doc """
-  A function that take a list of daily bird counts and return today's count.
+  Take a list of daily bird counts and return today's count.
   If the list is empty, it should return nil.
   """
   def today([]), do: nil
   def today([head | _]), do: head
   @spec increment_day_count(list(number)) :: nonempty_maybe_improper_list
   @doc """
-  A function that take a list of daily bird counts and increment the today's count by 1.
+  Take a list of daily bird counts and increment the today's count by 1.
   If the list is empty, return [1].
   """
   def increment_day_count([]), do: [1]
   def increment_day_count([head | tail]), do: [head + 1 | tail]
   @spec has_day_without_birds?(list(number)) :: boolean
   @doc """
-  A function that take a list of daily bird counts.
-  It should return true if there was at least one day when no birds visited the garden,
-  and false otherwise.
+  Take a list of daily bird counts. It should return true if there was
+  at least one day when no birds visited the garden, and false otherwise.
   """
   def has_day_without_birds?([]), do: false
   def has_day_without_birds?([head | _]) when head == 0, do: true
   def has_day_without_birds?([_ | tail]), do: BirdCount.has_day_without_birds?(tail)
   @spec total(list(number)) :: number
   @doc """
-  A function that take a list of daily bird counts and return the total number that
+  Take a list of daily bird counts and return the total number that
   visited your garden since you started collecting the data.
   """
   def total([]), do: 0
   def total([head | tail]), do: head + BirdCount.total(tail)
   @spec busy_days(list(number)) :: number
   @doc """
-  A function that take a list of daily bird counts and return the number of busy days.
+  Take a list of daily bird counts and return the number of busy days.
   """
   def busy_days([]), do: 0
   def busy_days([head | tail]) when head >= 5, do: 1 + BirdCount.busy_days(tail)
--- lib/high_school_sweetheart.ex
+++ lib/high_school_sweetheart.ex
@@ -4,7 +4,7 @@ defmodule HighSchoolSweetheart do
   by generating an ASCII heart with their initials:
   """
   @doc """
-  A function that take a name and return its first letter.
+  Take a name and return its first letter.
   It should clean up any unnecessary whitespace from the name.
   """
   def first_letter(name) do
@@ -13,7 +13,7 @@ defmodule HighSchoolSweetheart do
     |>String.first
   end
   @doc """
-  A function take a name and return its first letter, uppercase,
+  Take a name and return its first letter, uppercase,
   followed by a dot. Make sure to reuse HighSchoolSweetheart.first_letter/1
   """
   def initial(name) do
@@ -23,7 +23,7 @@ defmodule HighSchoolSweetheart do
     |>Kernel.<>(".")
   end
   @doc """
-  A function that take a full name, consisting of a first name and a last name
+  Take a full name, consisting of a first name and a last name
   separated by a space, and return the initials.
   """
   def initials(full_name) do
@@ -31,7 +31,7 @@ defmodule HighSchoolSweetheart do
     "#{HighSchoolSweetheart.initial(name)} #{HighSchoolSweetheart.initial(surname)}"
   end
   @doc """
-  A function take two full names and return the initials
+  Take two full names and return the initials
   """
   def pair(full_name1, full_name2) do
     name1 = HighSchoolSweetheart.initials(full_name1)
